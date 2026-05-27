335692e3d81e8ed28023c78bb334e10b3505052d
xingchen.li@audi.com.cn <V2SE7PP@cn.vwgroup.com>
2026-05-27 10:50:57 +0800

    feat: scaffold Vue 3 + Element Plus frontend project

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: '../sc_gr_app/web',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173
+  }
+})
